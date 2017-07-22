652b3b5bdd0ef0e6111fb80ec59f317ac5f270d4
Ayushi Saxena <ayusaxena96@gmail.com>
2017-07-22 19:27:22 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,7 @@
+First_list = ['Mango' , 'Apple' , 'Watermelon' , 'Orange']
+print("First Item", First_list[0])
+First_list[0] = 'Rasberry'
+print(First_list[0:3])
+Second_list = ['Tomatoes' , 'Carrots']
+Final_list = (First_list , Second_list)
+print(Final_list)
