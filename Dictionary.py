f0574a3532583c26611d6966ed75460c89f7ac5b
Ayushi Saxena <ayusaxena96@gmail.com>
2017-07-22 22:10:56 +0530

    Delete Dictionary.py

--- Dictionary.py
+++
@@ -1,6 +0,0 @@
-heroes = {'Tom Cruise': 'MI','Johnny Depp' : 'POC','Tom Hanks' : 'SPR'}
-print(heroes['Johnny Depp'])
-print(len(heroes))
-print(heroes.get("Tom Hanks"))
-print(heroes.keys())
-print(heroes.values())
